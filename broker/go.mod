c8dc737961235772babb49b3514a5a84a0811dbc
Gustavo Saboya <saboya.gustavo@gmail.com>
2023-12-05 23:03:42 -0300

    feat: (wip) defines entities related to investors and their asset positions

---
+++ broker/go.mod
@@ -0,0 +1,3 @@
+module github.com/saboyagustavo/code_invest
+
+go 1.21.4
---
+++ broker/internal/market/entity/investor.go
@@ -0,0 +1,19 @@
+package entity
+
+type Investor struct {
+	ID            string
+	Name          string
+	AssetPosition []*InvestorAssetPosition
+}
+
+func NewInvestor(id string) *Investor {
+	return &Investor{
+		ID:            id,
+		AssetPosition: []*InvestorAssetPosition{},
+	}
+}
+
+type InvestorAssetPosition struct {
+	AssetID string
+	Shares  int
+}
