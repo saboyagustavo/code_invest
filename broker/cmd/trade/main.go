eed385b8d60efc68346ddd89fc007d9e64687ceb
Gustavo Saboya <saboya.gustavo@gmail.com>
2023-12-08 18:14:23 -0300

    feat: main program - receives from kafka ch process messages then publishes it

---
+++ broker/cmd/trade/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"sync"
+
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/saboyagustavo/code_invest/internal/infra/kafka"
+	"github.com/saboyagustavo/code_invest/internal/market/dto"
+	"github.com/saboyagustavo/code_invest/internal/market/entity"
+	"github.com/saboyagustavo/code_invest/internal/market/transformer"
+)
+
+func main() {
+	ordersIn := make(chan *entity.Order)
+	ordersOut := make(chan *entity.Order)
+	wg := &sync.WaitGroup{}
+	defer wg.Wait()
+
+	kafkaMsgChan := make(chan *ckafka.Message)
+	configMap := &ckafka.ConfigMap{
+		// TODO: add to .env
+		"bootsrap.servers":  "host.docker.internal:9094",
+		"group.id":          "myGroup",
+		"auto.offset.reset": "earliest",
+	}
+
+	producer := kafka.NewProducer(configMap)
+	kafka := kafka.NewConsumer(configMap, []string{"input"})
+
+	go kafka.Consume(kafkaMsgChan) // Thread 2
+
+	/*
+		receives from kafka channel,
+		puts it in input,
+		process it,
+		puts in output
+		then publishes it in Kafka
+	*/
+	book := entity.NewBook(ordersIn, ordersOut, wg)
+	go book.Trade() // Thread 3
+
+	go func() {
+		for msg := range kafkaMsgChan {
+			wg.Add(1)
+			tradeInput := dto.TradeInput{}
+			err := json.Unmarshal(msg.Value, &tradeInput)
+			if err != nil {
+				panic(err)
+			}
+
+			order := transformer.TransformInput(tradeInput)
+			ordersIn <- order
+		}
+	}()
+
+	for res := range ordersOut {
+		output := transformer.TransformOutput(res)
+		outputJson, err := json.MarshalIndent(output, "", "  ")
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		producer.Publish(outputJson, []byte("orders"), "output")
+	}
+}
