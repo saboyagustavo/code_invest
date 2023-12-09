6679bae2ceb8ed35ee24ce0848596972f4217dd9
Gustavo Saboya <saboya.gustavo@gmail.com>
2023-12-09 09:28:45 -0300

    renames broker to market

---
+++ market/cmd/trade/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"sync"
+
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/saboyagustavo/code_invest/internal/infra/kafka"
+	"github.com/saboyagustavo/code_invest/internal/market/dto"
+	"github.com/saboyagustavo/code_invest/internal/market/entity"
+	"github.com/saboyagustavo/code_invest/internal/market/transformer"
+)
+
+func main() {
+	ordersIn := make(chan *entity.Order)
+	ordersOut := make(chan *entity.Order)
+	wg := &sync.WaitGroup{}
+	defer wg.Wait()
+
+	kafkaMsgChan := make(chan *ckafka.Message)
+	configMap := &ckafka.ConfigMap{
+		"bootstrap.servers": "host.docker.internal:9094",
+		"group.id":          "myGroup",
+		"auto.offset.reset": "latest",
+	}
+
+	producer := kafka.NewProducer(configMap)
+	kafka := kafka.NewConsumer(configMap, []string{"input"})
+
+	go kafka.Consume(kafkaMsgChan) // T2
+
+	/*
+		receives from kafka channel,
+		puts it in input,
+		process it,
+		puts in output
+		then publishes it in Kafka
+	*/
+	book := entity.NewBook(ordersIn, ordersOut, wg)
+	go book.Trade() // T3
+
+	go func() {
+		for msg := range kafkaMsgChan {
+			wg.Add(1)
+			fmt.Println(string(msg.Value))
+			tradeInput := dto.TradeInput{}
+			err := json.Unmarshal(msg.Value, &tradeInput)
+			if err != nil {
+				panic(err)
+			}
+			order := transformer.TransformInput(tradeInput)
+			ordersIn <- order
+		}
+	}()
+
+	for res := range ordersOut {
+		output := transformer.TransformOutput(res)
+		outputJson, err := json.MarshalIndent(output, "", "  ")
+		fmt.Println(string(outputJson))
+		if err != nil {
+			fmt.Println(err)
+		}
+		producer.Publish(outputJson, []byte("orders"), "output")
+	}
+}
---
+++ market/go.mod
@@ -0,0 +1,16 @@
+module github.com/saboyagustavo/code_invest
+
+go 1.21.4
+
+require (
+	github.com/confluentinc/confluent-kafka-go v1.9.2
+	github.com/google/uuid v1.4.0
+	github.com/stretchr/testify v1.8.4
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/confluentinc/confluent-kafka-go.v1 v1.8.2 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
